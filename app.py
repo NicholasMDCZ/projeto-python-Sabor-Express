2ca51ff72d74303eab95e56c1c5109acc1bb9eaf
Nicholas Castro <contabrpt2@hotmail.com>
2023-12-21 09:25:54 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ print('2. Listar restaurante')
 print('3. Ativar restaurante')
 print('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
+opcao_escolhida = int(input('Escolha uma opção: '))
 
 if opcao_escolhida == 1:
     print('Cadastrar restaurante')
