f679627b6c171716f6e8f6347e1b67fde9a8eb1a
Nicholas Castro <contabrpt2@hotmail.com>
2023-12-21 09:03:45 -0300

    Add files via upload

---
+++ exercicios.py
@@ -0,0 +1,12 @@
+print('Python na Escola de programação da Alura.')
+
+nome = 'Nicholas'
+idade = 24
+
+print(f'Meu nome é {nome} e tenho {idade} anos')
+
+print('A\nL\nU\nR\nA')
+
+pi = 3.14159
+
+print(f'O valor arredondado de pi é: {pi}')
