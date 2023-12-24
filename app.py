4a477a57008de5083d8293a27391df707ade491b
Nicholas Castro <contabrpt2@hotmail.com>
2023-12-24 17:45:10 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -44,6 +44,24 @@ def cadastrar_novo_restaurante():
         
         voltar_ao_menu_principal()        
 
+def alternar_estado_restaurante():
+    exibir_subtitulo('Alterando estado do restaurante')
+    nome_restaurante = input('Digite o nome do restaurante que você deseja alternar o estado: ')
+    restaurante_encontrado = False
+
+    for restaurante in restaurantes:
+        if nome_restaurante == restaurante['nome']:
+            restaurante_encontrado = True
+            restaurante['ativo'] = not restaurante['ativo']
+            mensagem = f'O restaurante {nome_restaurante} foi ativado com sucesso' if restaurante['ativo'] else f'O restaurante {nome_restaurante} foi desativado com sucesso'
+            print(mensagem)
+    if not restaurante_encontrado:
+        print('O restaurante não foi encontrado')
+
+
+
+    voltar_ao_menu_principal()
+
 def listar_restaurantes():
     exibir_subtitulo('Listando restaurantes')
     
@@ -65,7 +83,7 @@ def escolher_opcoes():
         elif opcao_escolhida == 2:
             listar_restaurantes()
         elif opcao_escolhida == 3:
-            print('Ativar Restaurante')
+            alternar_estado_restaurante()
         elif opcao_escolhida == 4:
             finalizar_app()
         else:
