be9d3654a3781666ee10c6a799cae0514dc6505e
Nicholas Castro <contabrpt2@hotmail.com>
2024-01-09 21:59:53 -0300

    Add files via upload

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,13 +1,30 @@
 class Restaurante:
+    restaurantes = []
+
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        ativo = False
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
+        Restaurante.restaurantes.append(self)
 
-restaurante_praca = Restaurante('Praça', 'Gourmet')
-restaurante_pizza = Restaurante('Pizza Express', 'Italiano')
+    def __str__(self):
+        return f'{self._nome} {self._categoria}'
+    
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{"Nome do restaurante".ljust(25)} | {"Categoria".ljust(25)} | {"Status"}')
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
 
-restaurantes = [restaurante_praca, restaurante_pizza]
+    @property
+    def ativo(self):
+        return '✅' if self._ativo else '❌'
+    
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
-print(restaurante_praca)
-print(restaurante_pizza)
+restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_praca.alternar_estado()
+restaurante_pizza = Restaurante('pizza Express', 'Italiano')
+
+Restaurante.listar_restaurantes()
